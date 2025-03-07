daf46e21e828658f5c53ececc3e00b33c731c82a
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-07 19:06:19 +0200

    142

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import tseslint from "typescript-eslint";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -8,12 +8,8 @@ export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  // ...tseslint.configs.recommended,
 ];
-
-
-
-
-
 
 
 
