c6e37f8b5f189de51c368b908e3d6d3649e7da8e
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-07 17:07:50 +0200

    142

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'airbnb',
+        'plugin:@typescript-eslint/recommended',
+    ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  }
+};
