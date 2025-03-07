48e296d241a8e2338675519a12affe393c7ced76
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-07 20:36:21 +0200

    142

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,18 @@ import path from 'path'
 export default defineConfig({
   
   optimizeDeps: {
-    include: ['handlebars'] // добавить зависимость handlebars
+    include: ['handlebars'] 
     
   },
 
   build:{
     outDir:path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
-    
-    
   },
   
   plugins: [
     handlebars({
       partialDirectory: path.resolve(__dirname, 'src/partials'),
-
       context: {
         title: 'Hello Yandex Practicum!',
       },
