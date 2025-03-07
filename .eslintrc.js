f999eb43fe9956b87a8109b9b4a476a95b923e93
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-07 19:17:34 +0200

    142

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
+const { TRUE } = require("sass-embedded");
+
 module.exports = {
+  env:{
+    node: true,
+    es6: TRUE
+  },
     parser: '@typescript-eslint/parser',
-    plugins:["@typescript-eslint"],
+    plugins:'@typescript-eslint/eslint-plugin',
     extends: [
         'airbnb',
-        'plugin:@typescript-eslint/recommended',
     ],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+  },
+  "rules":{
+    "max-len": [2, 100],
+    "@typescript-eslint/nounused-vars":2
   }
 };
 
