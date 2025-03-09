cebbbbcf4e2f6df5d26093284b2b834871bd7f61
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-09 20:15:21 +0200

    142

--- src/Auth/register.ts
+++ src/Auth/register.ts
@@ -7,24 +7,24 @@ export function setRegister(element: HTMLDivElement) {
             <h1>Регистрация</h1>
             <div class="form-group">
                 <label for="email">Почта:</label>
-                <input type="email" id="email" pattern = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/"name="email" placeholder="Введите вашу почту">
+                <input type="email" id="email" pattern = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,}$/"name="email" placeholder="Введите вашу почту">
             </div>
             <div class="form-group">
                 <label for="login">Ваш логин:</label>
-                <input type="text" id="login" pattern='(?=.*[a-z]|[A-Z])[a-zA-Z0-9\-_]{3,20}' name="login" placeholder="Введите ваш логин" >
+                <input type="text" id="login" pattern='(?=.*[a-z]|[A-Z])[a-zA-Z0-9-_]{3,20}' name="login" placeholder="Введите ваш логин" >
             </div>
             <div class="form-group">
                 <label for="first_name">Ваше имя:</label>
-                <input type="text" id="first_name" pattern ='^[A-ZА-Я]+[A-Za-zА-Яа-я\-]*' name="first_name" placeholder="Введите ваше имя:" >
+                <input type="text" id="first_name" pattern ='^[A-ZА-Я]+[A-Za-zА-Яа-я-]*' name="first_name" placeholder="Введите ваше имя:" >
             </div>
             <div class="form-group">
                 <label for="last_name">Ваше фамилия:</label>
-                <input type="text" id="last_name" pattern = '^[A-ZА-Я]+[A-Za-zА-Яа-я\-]*' name="last_name" placeholder="Введите ваше фамилия:" >
+                <input type="text" id="last_name" pattern = '^[A-ZА-Я]+[A-Za-zА-Яа-я-]*' name="last_name" placeholder="Введите ваше фамилия:" >
             </div>
              <div class="form-group">
                 <label for="phone">Ваш номер телефона:</label>
                 <input type="tel" id="phone" name="phone"
-                 placeholder="Введите ваш номер телефона:"required pattern = '^[\+]?[0-9]{10,15}'>
+                 placeholder="Введите ваш номер телефона:"required pattern = '^[+]?[0-9]{10,15}'>
             </div>
           <label class="input-file">
 	   	<input type="file" id="avatar" name="avatar" >
@@ -32,12 +32,12 @@ export function setRegister(element: HTMLDivElement) {
  	</label>
             <div class="form-group">
                 <label for="password">Ваш пароль:</label>
-                <input type="password" id="password" name="password" placeholder="Create a password" required pattern = '((?=.*\d)(?=.*[A-Z]).{8,40})' >
+                <input type="password" id="password" name="password" placeholder="Create a password" required pattern = '((?=.*d)(?=.*[A-Z]).{8,40})' >
                 <span class="show-password" onclick="togglePassword()">Show</span>
             </div>
             <div class="form-group">
                 <label for="password_again">Повторите пароль:</label>
-                <input type="password" id="password_again" name="password_again" pattern = '((?=.*\d)(?=.*[A-Z]).{8,40})' placeholder="Повторите пароль" required>
+                <input type="password" id="password_again" name="password_again" pattern = '((?=.*d)(?=.*[A-Z]).{8,40})' placeholder="Повторите пароль" required>
 
             </div>
             <button type="submit" id="register-button">Зарегистрироваться</button>
