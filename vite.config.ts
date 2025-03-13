8614384851091be625dc535295fa38fb38169238
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-13 17:35:05 +0300

    142

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import handlebars from 'vite-plugin-handlebars';
 import path from 'path';
-
 export default {
 
   optimizeDeps: {
     include: ['handlebars'],
 
   },
-
   build: {
     outDir: path.resolve('__dirname', 'dist'),
     emptyOutDir: true,
