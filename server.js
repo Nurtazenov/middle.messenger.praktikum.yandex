2cf5b1b71b536d7ad8101504d3ef895e9c84e5d3
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-20 17:24:19 +0300

    142

---
+++ server.js
@@ -0,0 +1,14 @@
+import express  from "express";
+import path from 'path'
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join("__dirname", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join("__dirname", "./dist/index.html"));
+});
+
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
