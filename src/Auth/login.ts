64f14e014cf6e2b04ead9df95641453e95937d0a
Nurtazenov <a.nurtazenov@yandex.ru>
2025-03-24 06:00:50 +0300

    142

--- src/Auth/login.ts
+++ src/Auth/login.ts
@@ -4,17 +4,14 @@ export function setLogin(element: HTMLDivElement) {
    <div class="container">
         <form action ="" method = "POST" id="getLog">
             <h1>Авторизация</h1>
-
             <div class="form-group">
                 <label for="email">Ваш логин:</label>
                 <input type="email" id="email" name="email" placeholder="Enter your email" required>
             </div>
 
             <div class="form-group">
-                <label for="password">Ваш пароль
-                </label>
+                <label for="password">Ваш пароль</label>
                 <input type="password" id="password" name="password" placeholder="Create a password" required>
-
             </div>
             <button type="submit" id="register-button">Авторизоваться</button>
             <a>Нет аккаунта?</a>
--- src/Auth/register.ts
+++ src/Auth/register.ts
@@ -7,32 +7,29 @@ export function setRegister(element: HTMLDivElement) {
             <h1>Регистрация</h1>
             <div class="form-group">
                 <label for="email">Почта:</label>
-                <input type="email" id="email" pattern = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/"name="email" placeholder="Введите вашу почту">
+                <input type="email" id="email" pattern = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/"name="email" placeholder="Введите вашу почту" required>
             </div>
             <div class="form-group">
                 <label for="login">Ваш логин:</label>
-                <input type="text" id="login" pattern='(?=.*[a-z]|[A-Z])[a-zA-Z0-9\\-_]{3,20}' name="login" placeholder="Введите ваш логин" >
+                <input type="text" id="login" pattern='(?=.*[a-z]|[A-Z])[a-zA-Z0-9\\-_]{3,20}' name="login" placeholder="Введите ваш логин" required >
             </div>
             <div class="form-group">
                 <label for="first_name">Ваше имя:</label>
-                <input type="text" id="first_name" pattern ='^[A-ZА-Я]+[A-Za-zА-Яа-я\\-]*' name="first_name" placeholder="Введите ваше имя:" >
+                <input type="text" id="first_name" pattern ='^[A-ZА-Я]+[A-Za-zА-Яа-я\\-]*' name="first_name" placeholder="Латиница или кириллица, первая буква должна быть заглавной" required >
             </div>
             <div class="form-group">
                 <label for="last_name">Ваше фамилия:</label>
-                <input type="text" id="last_name" pattern = '^[A-ZА-Я]+[A-Za-zА-Яа-я\\-]*' name="last_name" placeholder="Введите ваше фамилия:" >
+                <input type="text" id="last_name" pattern = '^[A-ZА-Я]+[A-Za-zА-Яа-я\\-]*' name="last_name" placeholder="Латиница или кириллица, первая буква должна быть заглавной" required>
             </div>
              <div class="form-group">
                 <label for="phone">Ваш номер телефона:</label>
                 <input type="tel" id="phone" name="phone"
-                 placeholder="Введите ваш номер телефона:"required pattern = '^[\\+]?[0-9]{10,15}'>
+                 placeholder=" от 10 до 15 символов, состоит из цифр, может начинается с плюса."required pattern = '^[\\+]?[0-9]{10,15}' >
             </div>
-          <label class="input-file">
-	   	<input type="file" id="avatar" name="avatar" >
- 	   	<span class="input-file-btn">Фотография вашего аватара</span>           
- 	</label>
+         
             <div class="form-group">
                 <label for="password">Ваш пароль:</label>
-                <input type="password" id="password" name="password" placeholder="Create a password" required pattern = '((?=.*\\d)(?=.*[A-Z]).{8,40})' >
+                <input type="password" id="password" name="password" placeholder="от 8 до 40 символов, обязательно хотя бы одна заглавная буква и цифра" required pattern = '((?=.*\\d)(?=.*[A-Z]).{8,40})' >
                 <span class="show-password" onclick="togglePassword()">Show</span>
             </div>
             <div class="form-group">
