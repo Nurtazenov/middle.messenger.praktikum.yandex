ff867119bbf29a245a7ff591de54ebb1f4c822bd
Nurtazenov <a.nurtazenov@yandex.ru>
2025-07-21 07:01:50 +0300

    3.3

--- src/Navbar/Navbar.ts
+++ src/Navbar/Navbar.ts
@@ -1,3 +1,4 @@
+
 document.querySelector<HTMLDivElement>('#nav')!.innerHTML = `
     <div class = "main" style="display: flex; margin:10px auto; flex-direction: row; bottom:100px; justify-content: center;">
         <h2 class = "title" style="color:green">
