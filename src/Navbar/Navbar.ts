5674c771587607a70fff25345fff1fc469a0e527
Nurtazenov <a.nurtazenov@yandex.ru>
2025-07-21 07:01:06 +0300

    3.3

--- src/Navbar/Navbar.ts
+++ src/Navbar/Navbar.ts
@@ -1,14 +1,9 @@
-import './navbar.scss';
-
 document.querySelector<HTMLDivElement>('#nav')!.innerHTML = `
-    <div class = "main">
-        <h2
-     class = "title">
+    <div class = "main" style="display: flex; margin:10px auto; flex-direction: row; bottom:100px; justify-content: center;">
+        <h2 class = "title" style="color:green">
         Messenger
         </h2>        
-         </div>`
-         ;
+         </div>`;
     
 
     
-
