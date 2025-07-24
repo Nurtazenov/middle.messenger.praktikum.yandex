4b254bfe1749e0a6d1de8b78a5f58bd8d5cd7b3e
Nurtazenov <a.nurtazenov@yandex.ru>
2025-07-24 18:35:55 +0300

    3

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,16 @@ import path, {resolve} from 'path';
 import vitePluginHandlebarsPrecompile from './vite-plugin-handlebars-precompile';
 import eslintPlugin from "vite-plugin-eslint";
 import svgr from "vite-plugin-svgr";
-export default {
+import { defineConfig } from 'vite';
+export default defineConfig({
   plugins: [
-    eslintPlugin({
-      include: ["/**/*.ts", "/**/*.js", "/**/*.hbs"],
-    }),
     handlebars({
-      partialDirectory: resolve(__dirname, 'src/partials'),
+      partialDirectory: resolve(__dirname, "src/partials"),
     }),
     vitePluginHandlebarsPrecompile(),
     svgr(),
   ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['legacy-js-api'],
-      }
-    },
-  },
- build: {
+  build: {
     outDir: "./dist",
     sourcemap: true,
     rollupOptions: {
@@ -32,11 +23,18 @@ export default {
         dir: "./dist",
         format: "es",
       },
-      external: ['/src/Navbar/navbar.ts',"/src/main.ts"]
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/variables.scss";`,
+      },
     },
   },
   server: {
     port: 3000,
-  }
-};
-
+    open: true,
+  },
+});
