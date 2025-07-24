a2613f4b1d003906ee523fa8acd8f3714f36fdf8
Nurtazenov <a.nurtazenov@yandex.ru>
2025-07-24 17:53:51 +0300

    3

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import express  from "express";
+import path from 'path'
 
 const app = express();
 const PORT = 3000;
@@ -13,8 +8,6 @@ app.use(express.static(path.join("dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join("./dist/index.html"));
-  console.log(path.join(__dirname, "./dist/index.html"));
-  res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
