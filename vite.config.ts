35a7f26830ffcb50eb2fa007efc9794871939b67
Nurtazenov <a.nurtazenov@yandex.ru>
2025-07-24 17:47:21 +0300

    3

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default {
       input: {
         main: path.resolve(__dirname, 'index.html'),
       },
-      external: ['/src/Navbar/navbar.ts',"/src/main.ts"]
+
     },
   },
   server: {
