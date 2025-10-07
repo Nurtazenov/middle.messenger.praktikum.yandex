dd2e9a155f2c49e808cedac2c7c9c5346efc9216
Nurtazenov <a.nurtazenov@yandex.ru>
2025-10-08 02:11:34 +0300

    Мой месседж с кодом без проверки

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
